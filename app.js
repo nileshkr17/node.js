322037656c2c52697bf4c1713bd5fb80bd198ff7
Nilesh <ece.20becd76@silicon.ac.in>
2022-01-20 21:52:27 +0530

    [fixed]

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title><head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('<body><h1>Hello from my Node Server!</h1></body>');
   res.write('</html>');
   res.end();
 });
