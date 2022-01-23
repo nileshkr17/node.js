a94435116cfafc78e7703d74aafc222255235e17
Nilesh <ece.20becd76@silicon.ac.in>
2022-01-23 15:42:06 +0530

    [express-operation-added]

--- node-json-nodemon1/app.js
+++ node-json-nodemon1/app.js
@@ -3,6 +3,17 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
+app.use((req,res,next)=>
+{
+    console.log('In the middleware!');
+    //res.send('hello');
+    next(); //Allows the request to continue to the next middleware in line
+});
+app.use((req,res,next)=>
+{
+   res.send('hi');
+    console.log('In another middleware!');
+})
 
 const server = http.createServer(app);
 
