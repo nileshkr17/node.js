df4b39f450501eb90349b8ca3c2e1f1016f6b539
Nilesh <ece.20becd76@silicon.ac.in>
2022-01-23 13:01:55 +0530

    .

--- app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-const routesNew = require('./routes');
-const server = http.createServer(routesNew.handler);
-console.log(routesNew.someText);
-
-
-server.listen(3000);
