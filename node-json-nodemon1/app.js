2fa9686e639caa30425329fd019c4f94789a4333
Nilesh <ece.20becd76@silicon.ac.in>
2022-01-30 20:44:11 +0530

    []'

--- node-json-nodemon1/app.js
+++ node-json-nodemon1/app.js
@@ -4,17 +4,17 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req,res,next)=>
+app.use('/add-producthere', (req,res,next)=>
 {
     res.send('<form action ="/product" method="POST"> <input type="text" name="title"><button type="submit">Add Product</form>');
      //Allows the request to continue to the next middleware in line
 });
-app.use('/product', (req,res,next)=> {
+app.get('/product', (req,res,next)=> {
     console.log(req.body);
     res.redirect('/');
 });
 
-app.use('/', (req,res,next)=>{
+app.get('/', (req,res,next)=>{
     res.send('<h1>Hello from NodeJs.</h1>');
 });
 
